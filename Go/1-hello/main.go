bcde5c03e8b296add1817692fef6475d70d41ff0
Jasmin Kovacevic <fire23332@gmail.com>
2025-05-29 22:41:00 +0200

    init others

---
+++ Go/1-hello/main.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	var msutafa int8
+	msutafa = 50
+	for msutafa > 40{
+		fmt.Println("Hello World")
+		msutafa += 10
+		fmt.Println("Msutafa is", msutafa)
+	}
+}
+
