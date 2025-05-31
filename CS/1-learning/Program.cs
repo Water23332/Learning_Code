7f4043fb534a25624621c2c9a53b1f6ceb5b3698
Water23332 <water23332@gmail.com>
2025-05-31 10:59:45 +0200

    init lesson 5

--- CS/1-learning/Program.cs
+++ CS/1-learning/Program.cs
@@ -1,8 +1,10 @@
 ﻿
 
-Console.WriteLine("Hello, David!");
+Console.WriteLine("1. Hello, David!");
 
-string friend1 = "Msutafa";
-string friend2 = "Maxx";
+string friend1 = "    Msutafa   ";
+string friend2 = "////Maxx///";
+friend2 = friend2.Trim('/');
 
-Console.WriteLine($"Hello {friend1} and {friend2}");
+Console.WriteLine($"2. Hello {friend1.Trim()} and {friend2}.");
+Console.WriteLine("3. Hello " + friend1 + " and " + friend2 + ". You BASTARDS!");
