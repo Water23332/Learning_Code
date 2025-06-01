4e7e8a1b39d7f9641f3024e24b1cb64da0da1444
Water23332 <water23332@gmail.com>
2025-06-01 12:36:16 +0200

    asd

--- CS/1-learning/main.cs
+++ CS/1-learning/main.cs
@@ -1,1 +1,7 @@
-﻿
+﻿// following https://www.youtube.com/watch?v=UETol82cPZk&list=PLdo4fOcmZ0oULFjxrOagaERVAMbmG20Xe&index=9
+// and this in text form https://learn.microsoft.com/en-us/dotnet/csharp/tour-of-csharp/tutorials/branches-and-loops
+
+int a = 5;
+int b = 6;
+if (a + b > 10) 
+    Console.WriteLine($"1. The answer is greater than 10. ITS {a + b}!!!");
