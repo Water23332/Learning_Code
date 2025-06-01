f0c0027404602412305735f8f328188f540e1b49
Water23332 <water23332@gmail.com>
2025-06-01 12:53:21 +0200

    asdasd

--- Go/1-hello/main.go
+++ Go/1-hello/main.go
@@ -5,10 +5,9 @@ import "fmt"
 func main() {
 	var msutafa int8
 	msutafa = 50
-	for msutafa > 40{
+	for msutafa > 40 {
 		fmt.Println("Hello World")
 		msutafa += 10
 		fmt.Println("Msutafa is", msutafa)
 	}
 }
-
